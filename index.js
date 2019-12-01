d1ec6fb99dcfec89e14d8ed76cfc9475c2fc7e34
AbuSufianIslam <thebooming1992@gmail.com>
2019-11-30 19:57:06 -0500

    complete

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route, Link } from 'react-router-dom';
 
 const Red = (props) => {
-  return (
-    <div className='red'>
-      <h1>RED</h1>
-    </div>
-  )
-}
+	return (
+		<div className="red">
+			<h1>RED</h1>
+		</div>
+	);
+};
 
 const Blue = (props) => {
-  return (
-    <div className='blue'>
-      <h1>BLUE</h1>
-    </div>
-  )
-}
+	return (
+		<div className="blue">
+			<h1>BLUE</h1>
+		</div>
+	);
+};
 
 const Main = (props) => {
-  return (
-    <div id='container'>
+	return (
+		<HashRouter>
+			<div id="container">
+				<div id="navbar">
+					<Link to="/blue">Go to Blue</Link>
+					<Link to="/red">Go to Red</Link>
+				</div>
 
-      <div id='navbar'>
-        {/* navigation here */}
-      </div>
+				<div id="main-section">
+					<Route path="/blue" component={Blue} />
+					<Route path="/red" component={Red} />
+					<Route exact path="/" component={Red} />
+				</div>
+			</div>
+		</HashRouter>
+	);
+};
 
-      <div id='main-section'>
-        {/* routes here */}
-      </div>
-    </div>
-  )
-}
-
-const app = document.getElementById('app')
-ReactDOM.render(<Main />, app)
+const app = document.getElementById('app');
+ReactDOM.render(<Main />, app);
